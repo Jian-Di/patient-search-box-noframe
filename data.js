6370af91dea83724896d6e26fc2ece4e98c9fff7
Jian <b101105068@tmu.edu.tw>
2022-09-29 14:30:28 +0800

    完成搜尋欄的建議清單功能

---
+++ data.js
@@ -0,0 +1,22 @@
+
+const simPatient1 = {
+    dialoge: {
+        history: "none",
+        symptom: "none",
+    },
+    pe: {
+        gcs: "E4V5M6",
+        breathingSound: "Bilateral clear breathing sound",
+        heartSound: "Regular heart beat",
+    },
+    labStudy: {
+        CBC: "xxx"
+    },
+    image: {
+        chestXRay: "xxx",
+        chestCT: "xxx",
+    },
+    monitor: {
+        vitalSign: "xxx",
+    }
+};
--- index.js
+++ index.js
@@ -1,29 +1,58 @@
-const selectEl = document.getElementById("select");
-const listEl = document.getElementById("list");
-const optionsEl = document.getElementsByClassName("options");
-const selectTextEl = document.getElementById("selectText");
-const searchBarInputEl = document.getElementById("searchBarInput")
-
-
 /**
- * 可以透過點擊 menu item 來替換成當前的 Category
+ * 可以透過點擊 menu item 來將文字替換成當前的 Category
  */
+ const selectCateEl = document.getElementById("selectCate");
+ const cateListEl = document.getElementById("cateList");
+ const cateOptionsEl = document.getElementsByClassName("cateOptions");
+ const selectCateTextEl = document.getElementById("selectCateText");
+ const searchBarInputEl = document.getElementById("searchBarInput")
 
-selectEl.onclick = () => {
-    listEl.classList.toggle("open"); // 這好牛B...
+selectCateEl.onclick = () => {
+    cateListEl.classList.toggle("open");
 }
 
-for ( let optionEl of optionsEl) {
+for ( let optionEl of cateOptionsEl) {
     optionEl.onclick = () => {
-        selectTextEl.innerHTML = optionEl.innerHTML;
-        searchBarInputEl.placeholder = `Search in ${selectTextEl.innerHTML}`;
+        selectCateTextEl.innerHTML = optionEl.innerHTML;
+        searchBarInputEl.placeholder = `Search in ${selectCateTextEl.innerHTML}`;
     }  
 }
 
 /**
- * 讓 Search input 可以有建議的搜尋項目
- */
+ * 讓 Search Box 會根據打在搜尋欄的字，來篩選下方建議清單的內容
+ * 
+ * @searchTerm searchTerm 打在搜尋欄的字
+ * @searchList searchList 要篩選的清單，要是 List of DOM element
+*/
+const searchListEl = document.getElementById("searchList");
+
+const searchOptionsEl = document.getElementsByClassName("searchOptions");
+const currentSearchList = searchOptionsEl;
 
 searchBarInputEl.addEventListener("keyup", (e) => {
-    filterList(e.target.value);
+    e.target.value? searchListEl.classList.add("open") : searchListEl.classList.remove("open"); // Input 有文字時才顯示建議清單
+    filterList(e.target.value, currentSearchList);
 });
+
+const filterList = (searchTerm, searchList) => {
+    searchTerm = searchTerm.toLowerCase();
+    for (let listItem of searchList) {
+        let listItemText = listItem.innerText.toLowerCase();
+        if (listItemText.indexOf(searchTerm) !== -1) {
+            listItem.style.display = "block";
+        } else {
+            listItem.style.display = "none";
+        }
+    }
+};
+
+/**
+ * 點擊建議清單的 list item 之後，會將搜尋欄的 input value 替換成該 item
+ */
+for ( let listItem of currentSearchList) {
+    listItem.onclick = () => {
+        searchBarInputEl.value = listItem.innerHTML;
+        searchListEl.classList.remove("open");
+    }  
+}
+
